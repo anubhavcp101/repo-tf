8f06c5cffe5ceca11c55ca2791b54ac2c736cf86
anubhavcp101 <125131631+anubhavcp101@users.noreply.github.com>
2025-01-08 21:29:56 +0530

    Add files via upload

---
+++ Applications/rsv/main.tf
@@ -0,0 +1,65 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-rsvTest"
+  location = "West US"
+}
+
+resource "azurerm_recovery_services_vault" "vault" {
+  name                = "testVault"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  public_network_access_enabled = false
+  soft_delete_enabled = true
+
+  storage_mode_type = "LocallyRedundant"
+}
+
+resource "azurerm_site_recovery_replication_policy" "ReplicationPolicy" {
+  name                                                 = "myReplicationPolicy"
+  resource_group_name                                  = azurerm_resource_group.rg.name
+  recovery_vault_name                                  = azurerm_recovery_services_vault.vault.name
+  recovery_point_retention_in_minutes                  = 1440
+  application_consistent_snapshot_frequency_in_minutes = 60
+}
+
+resource "azurerm_backup_policy_vm" "backupPolicy" {
+  name                = "myBackupPolicy"
+  resource_group_name = azurerm_resource_group.rg.name
+  recovery_vault_name = azurerm_recovery_services_vault.vault.name
+
+  policy_type = "V2"
+  instant_restore_retention_days = 5
+
+  timezone = "UTC"
+
+  backup {
+    frequency = "Weekly"
+    time      = "21:00"
+    weekdays = ["Sunday"]
+  }
+/*
+  retention_daily {
+    count = 14
+  }
+  */
+
+
+  retention_weekly {
+    count    = 42
+    weekdays = ["Sunday", "Wednesday", "Friday", "Saturday"]
+  }
+/*
+  retention_monthly {
+    count    = 7
+    weekdays = ["Sunday", "Wednesday"]
+    weeks    = ["First", "Last"]
+  }
+
+  retention_yearly {
+    count    = 77
+    weekdays = ["Sunday"]
+    weeks    = ["Last"]
+    months   = ["January"]
+  }
+  */
+}
---
+++ Applications/rsv/provider.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+provider "tls" {
+  # Configuration options
+}
+provider "local" {
+  # Configuration options
+}
